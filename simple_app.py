38bfc21e7eb6df827ae95eee41b002bea2b7dd8e
Davide <dddomodossola@gmail.com>
2014-03-20 12:24:43 +0100

    Update simple_app.py

--- simple_app.py
+++ simple_app.py
@@ -6,20 +6,22 @@ class App( BaseApp ):
 		super( App, self ).__init__( *args )
 
 	def main( self ):
-		#the arguments are	width - height - layoutOrientationOrizontal
-		wid = gui.widget( 100, 60, False )
-		self.lbl = gui.labelWidget( 100, 30, "Hello world!" )
-		self.bt = gui.buttonWidget( 100, 30, "Press me!" )
+		#if this is the first time the application is instanced
+		if not hasattr( self.client, "root" ):
+			#the arguments are	width - height - layoutOrientationOrizontal
+			wid = gui.widget( 100, 60, False )
+			self.lbl = gui.labelWidget( 100, 30, "Hello world!" )
+			self.bt = gui.buttonWidget( 100, 30, "Press me!" )
 			
-		#setting the listener for the onclick event of the button
-		self.bt.setOnClickListener( self, "onButtonPressed" )
-		
-		#appending a widget to another, the first argument is a string key
-		wid.append( "1", self.lbl )
-		wid.append( "2", self.bt )
+			#setting the listener for the onclick event of the button
+			self.bt.setOnClickListener( self, "onButtonPressed" )
 			
-		#setting up the root widget
-		self.client.root = wid
+			#appending a widget to another, the first argument is a string key
+			wid.append( "1", self.lbl )
+			wid.append( "2", self.bt )
+			
+			#setting up the root widget
+			self.client.root = wid
 	
 	#listener function
 	def onButtonPressed( self, x, y ):
