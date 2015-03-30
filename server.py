b762060bbd2db9f2af07c6f70b69d8609cf74da4
Davide Rosa <d.rosa@imts.eu>
2015-03-30 15:14:24 +0200

    Another bugfix on the propagation of style and attachments in the server function that returns the page content. Style and attachments are propagated only in case of returning text/html content-type.

--- server.py
+++ server.py
@@ -174,9 +174,10 @@ class BaseApp(BaseHTTPRequestHandler,object):
 				self.send_response(200)
 				self.send_header('Content-type',ret[1])
 				self.end_headers()
-
-				self.wfile.write(self.client.attachments);
-				self.wfile.write("<link href='" + BASE_ADDRESS + "style.css' rel='stylesheet' />");
+				
+				if( ret[1] == 'text/html' ):
+					self.wfile.write(self.client.attachments);
+					self.wfile.write("<link href='" + BASE_ADDRESS + "style.css' rel='stylesheet' />");
 
 				self.wfile.write( ret[0] )
 				
