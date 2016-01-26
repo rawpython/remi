b41445980384b6afc72312039b121bbbcf7e6e2e
Davide <dddomodossola@gmail.com>
2016-01-27 00:13:42 +0100

    Update editor.py
    Changed editor app port

--- editor/editor.py
+++ editor/editor.py
@@ -526,4 +526,4 @@ if __name__ == "__main__":
     # starts the webserver
     # optional parameters
     # start(MyApp,address='127.0.0.1', port=8081, multiple_instance=False,enable_file_cache=True, update_interval=0.1, start_browser=True)
-    start(Editor, debug=False)
+    start(Editor, debug=False, port=8082)
