95c12278c99c81b38d4d4c4b5c1152517aafb986
allanderek <allan.clark@gmail.com>
2017-02-07 20:12:18 +0000

    Provide a proper command-line arguments entry point for examples/widgets_overview which I will now generalise to the other applications.

--- examples/widgets_overview_app.py
+++ examples/widgets_overview_app.py
@@ -343,4 +343,42 @@ if __name__ == "__main__":
     # optional parameters
     # start(MyApp,address='127.0.0.1', port=8081, multiple_instance=False,enable_file_cache=True, update_interval=0.1, start_browser=True)
 
-    start(MyApp, debug=True, address='0.0.0.0')
+    try:
+        import click
+
+        @click.command()
+        # We could of course specify defaults here, but we don't want to
+        # maintain defaults in both the `remi/server.py` and here, so what we
+        # do is *not* pass in any values that the user does not explicitly
+        # specify. The boolean flags we have to give a default of 'None' to
+        # *avoid* giving a True or False default.
+        @click.option('--start-browser/--no-start-browser', default=None)
+        @click.option('--debug/--no-debug', default=None)
+        @click.option('--address')
+        @click.option('--port', type=int)
+        @click.option('--websocket-protocol')
+        @click.option('--host-name')
+        @click.option('--websocket-port', type=int)
+        @click.option('--title')
+        @click.option('--username')
+        @click.option('--password')
+        @click.option('--multiple-instance/--no-multiple-instance', default=None)
+        @click.option('--enable_file_cache/--no-enable-file-cache', default=None)
+        @click.option('--update_interval', type=float)
+        @click.option('--websocket_timeout_timer_ms', type=int)
+        @click.option('--websocket_protocol')
+        @click.option('--pending_messages_queue_length', type=int)
+        def run(**kwargs):
+            kwargs = {name: value for name, value in kwargs.items() if value is not None}
+            start(MyApp, **kwargs)
+
+    except ImportError:
+        import sys
+        if len(sys.argv) > 1:
+            print("To give command-line arguments you need to install 'click'")
+            print("pip install click")
+            sys.exit(1)
+        def run():
+            start(MyApp, debug=True, address='0.0.0.0')
+
+    run()
