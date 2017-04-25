9e7dfe7134ff0e8e25fdc0f8791957b8ce3e68cf
RomanMeR <RomanMescheryakov@yandex.ru>
2017-04-25 12:04:05 +0300

    Use os.path.join for saved file path building in FileUploader

--- remi/gui.py
+++ remi/gui.py
@@ -2477,7 +2477,7 @@ class FileUploader(Widget):
         self.eventManager.register_listener(self.EVENT_ON_FAILED, callback, *userdata)
 
     def ondata(self, filedata, filename):
-        with open(self._savepath+filename, 'wb') as f:
+        with open(os.path.join(self._savepath, filename), 'wb') as f:
             f.write(filedata)
         return self.eventManager.propagate(self.EVENT_ON_DATA, (filedata, filename))
 
