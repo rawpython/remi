ced64c4b1e3b300b2b60aa7da62a286caabeb234
Snowy <bethany.lu@student.uts.edu.au>
2019-08-14 15:24:30 +1000

    Adding new version of label unit test. Fixed the exception bug.

---
+++ test/test_widget.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+    
+
+import unittest
+import remi.gui as gui
+
+
+class TestLabel(unittest.TestCase):
+    '''
+    Unit testing for Label class. This tests the instantiation of 
+    the class. It also test to see if there are any exceptions too.
+    '''
+    def test_init(self):
+        self.assertEqual(gui.Label('Testing').innerHTML({}), 'Testing')
+
+        different_types = [{}, [], 123, (4, 5)]
+        for each_type in different_types:
+            with self.assertRaises(Exception) as error:
+                gui.Label(each_type)
+
+if __name__ == '__main__':
+    unittest.main()
