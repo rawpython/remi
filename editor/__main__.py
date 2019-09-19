f8c9a295f78729b015e57df010147a2dbb20329a
Jesse Clark <jjclark1982@gmail.com>
2019-09-19 21:17:05 +1000

    support importing and executing editor as a python package

---
+++ editor/__main__.py
@@ -0,0 +1,5 @@
+from editor import Editor
+from remi import start
+
+if __name__ == "__main__":
+    start(Editor, debug=False, address='0.0.0.0', port=8082, update_interval=0.01, multiple_instance=True)
