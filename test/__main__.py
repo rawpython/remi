8de6aacd935d95ab0869b41f8fb9032f21e03445
Jesse Clark <jjclark1982@gmail.com>
2019-09-19 13:06:00 +1000

    update docstring in main test script

--- test/__main__.py
+++ test/__main__.py
@@ -5,10 +5,12 @@ import os.path
 import sys
 
 if __name__ == '__main__':
-    ''' Finds and runs the unit tests.
-
     '''
-    suite = unittest.defaultTestLoader.discover(os.path.dirname(__file__))
+    Find and run the unit tests.
+    This is equivalent to running:
+    python -m unittest discover -t remi/ -s remi/test/ -v
+    '''
 
+    suite = unittest.defaultTestLoader.discover(os.path.dirname(__file__))
     runner=unittest.TextTestRunner(verbosity=2)
     runner.run(suite)
