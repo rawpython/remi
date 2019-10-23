bb8baca809545b63018813d7e01af0ec898ba01b
Jesse Clark <jjclark1982@gmail.com>
2019-10-23 15:55:06 +1100

    run browser tests in both chrome and firefox, if installed

--- test/browser_test/test_append_and_remove_widget_app_gui.py
+++ test/browser_test/test_append_and_remove_widget_app_gui.py
@@ -8,56 +8,67 @@ examples_dir = os.path.realpath(os.path.join(os.path.abspath(\
                                 os.path.dirname(__file__)), '../../examples'))
 sys.path.append(examples_dir)
 from append_and_remove_widgets_app import MyApp
-try:
-    from selenium import webdriver
-except ImportError:
-    webdriver = None
 
+def load_webdriver(browser_name):
+    try:
+        from selenium import webdriver
+    except ImportError:
+        webdriver = None
 
-# This is the only code you need to edit in this script.
-# Enter in your Sauce Labs Credentials in order to run this test
-# This variable contains the service address for the Sauce Labs VM hub
+    try:
+        DriverClass = webdriver.get(browser_name)
+        OptionsClass = webdriver.get(str(browser_name)+"Options")
+        DriverClass()
+        return (webdriver, DriverClass, OptionsClass)
+    except:
+        return (webdriver, None, None)
 
-@unittest.skipIf(webdriver is None, "Skipping Selenium test.")
-class TestAppendAndRemoveWidgetApp(unittest.TestCase):
+def define_test_suite(browser_name=None):
+    (webdriver, DriverClass, OptionsClass) = load_webdriver(browser_name)
+    @unittest.skipIf(webdriver is None, "Selenium is not installed.")
+    @unittest.skipIf(DriverClass is None, "{} webdriver is not installed.".format(browser_name))
+    @unittest.skipIf(webdriver is None, "Skipping Selenium test.")
+    class TestAppendAndRemoveWidgetApp(unittest.TestCase):
 
-    def setUp(self):
-        self.server = remi.Server(MyApp, start=False, address='0.0.0.0',start_browser=False)
-        self.server.start()
-        # the desired_capabilities parameter tells us which browsers and OS to spin up.
-        # This creates a webdriver object to send to Sauce Labs including the desired capabilities
-        # self.driver = webdriver.Remote(command_executor=remote_url, desired_capabilities=desired_cap)
-        self.options = webdriver.ChromeOptions()
-        self.options.headless = True
-        self.driver = webdriver.Chrome(chrome_options=self.options)
-        self.driver.implicitly_wait(30)
+        def setUp(self):
+            self.server = remi.Server(MyApp, start=False, address='0.0.0.0',start_browser=False)
+            self.server.start()
+            self.options = webdriver.ChromeOptions()
+            self.options.headless = True
+            self.driver = webdriver.Chrome(chrome_options=self.options)
+            self.driver.implicitly_wait(30)
 
-    def test_should_open_chrome(self):
-        self.driver.get(self.server.address)
-        body = self.driver.find_element_by_tag_name('body')
-        self.assertIn('buttons', body.text)
+        def test_should_open_chrome(self):
+            self.driver.get(self.server.address)
+            body = self.driver.find_element_by_tag_name('body')
+            self.assertIn('buttons', body.text)
 
-    def test_button_press(self):
-        self.driver.get(self.server.address)
-        add_button = self.driver.find_elements_by_tag_name('button')[0]
-        remove_button = self.driver.find_elements_by_tag_name('button')[1]
-        empty_button = self.driver.find_elements_by_tag_name('button')[2]
+        def test_button_press(self):
+            self.driver.get(self.server.address)
+            add_button = self.driver.find_elements_by_tag_name('button')[0]
+            remove_button = self.driver.find_elements_by_tag_name('button')[1]
+            empty_button = self.driver.find_elements_by_tag_name('button')[2]
 
-        time.sleep(1.0)
-        for count in range(0,2):
-            add_button.send_keys('\n')
+            time.sleep(1.0)
+            for count in range(0,2):
+                add_button.send_keys('\n')
 
-        time.sleep(1.0)
-        remove_button.send_keys('\n')
-        # now we need to know that the right number of boxes have been
-        # generated
+            time.sleep(1.0)
+            remove_button.send_keys('\n')
+            # now we need to know that the right number of boxes have been
+            # generated
 
-    # Here we send the results to Sauce Labs and tear down our driver session
-    def tearDown(self):
-        # This is where you tell Sauce Labs to stop running tests on your behalf.
-        # It's important so that you aren't billed after your test finishes.
-        self.driver.quit()
-        self.server.stop()
+        # Here we send the results to Sauce Labs and tear down our driver session
+        def tearDown(self):
+            # This is where you tell Sauce Labs to stop running tests on your behalf.
+            # It's important so that you aren't billed after your test finishes.
+            self.driver.quit()
+            self.server.stop()
+
+    return TestAppendAndRemoveWidgetApp
+
+TestHelloWorldChrome = define_test_suite(browser_name="Chrome")
+TestHelloWorldFirefox = define_test_suite(browser_name="Firefox")
 
 if __name__ == '__main__':
     unittest.main(buffer=True)
--- test/browser_test/test_helloworld_app_gui.py
+++ test/browser_test/test_helloworld_app_gui.py
@@ -1,4 +1,3 @@
-# Selenium 3.14+ doesn't enable certificate checking
 import unittest
 import time
 import sys
@@ -8,57 +7,70 @@ examples_dir = os.path.realpath(os.path.join(os.path.abspath(\
                                 os.path.dirname(__file__)), '../../examples'))
 sys.path.append(examples_dir)
 from helloworld_app import MyApp
-try:
-    from selenium import webdriver
-except ImportError:
-    webdriver = None
+
+def load_webdriver(browser_name):
+    try:
+        from selenium import webdriver
+    except ImportError:
+        webdriver = None
+
+    try:
+        DriverClass = webdriver.get(browser_name)
+        OptionsClass = webdriver.get(str(browser_name)+"Options")
+        DriverClass()
+        return (webdriver, DriverClass, OptionsClass)
+    except:
+        return (webdriver, None, None)
+
+def define_test_suite(browser_name=None):
+    (webdriver, DriverClass, OptionsClass) = load_webdriver(browser_name)
+    @unittest.skipIf(webdriver is None, "Selenium is not installed.")
+    @unittest.skipIf(DriverClass is None, "{} webdriver is not installed.".format(browser_name))
+    class TestHelloWorld(unittest.TestCase):
+
+        def setUp(self):
+            self.server = remi.Server(MyApp, start=False, address='0.0.0.0',start_browser=False)
+            self.server.start()
 
 
+            self.options = OptionsClass()
+            self.options.headless = True
+            self.driver = DriverClass(chrome_options=self.options)
+            self.driver.implicitly_wait(30)
 
-# This is the only code you need to edit in this script.
-# Enter in your Sauce Labs Credentials in order to run this test
-# This variable contains the service address for the Sauce Labs VM hub
+        def test_should_open_chrome(self):
+            self.driver.get(self.server.address)
+            button = self.driver.find_element_by_tag_name('button')
+            self.assertTrue('Press me!' in button.text)
 
-@unittest.skipIf(webdriver is None, "Skipping Selenium test.")
-class TestHelloWorld(unittest.TestCase):
+        def test_button_press(self):
+            self.driver.get(self.server.address)
+            body = self.driver.find_element_by_tag_name('body')
+            # self.assertFalse('Hello World!' in body.text)
+            self.assertNotIn('Hello World!', body.text)
+            # print(body.text, "hereee")
 
-    def setUp(self):
-        self.server = remi.Server(MyApp, start=False, address='0.0.0.0',start_browser=False)
-        self.server.start()
-        self.options = webdriver.ChromeOptions()
-        self.options.headless = True
-        self.driver = webdriver.Chrome(chrome_options=self.options)
-        self.driver.implicitly_wait(30)
-
-    def test_should_open_chrome(self):
-        self.driver.get(self.server.address)
-        button = self.driver.find_element_by_tag_name('button')
-        self.assertTrue('Press me!' in button.text)
-
-    def test_button_press(self):
-        self.driver.get(self.server.address)
-        body = self.driver.find_element_by_tag_name('body')
-        # self.assertFalse('Hello World!' in body.text)
-        self.assertNotIn('Hello World!', body.text)
-        # print(body.text, "hereee")
-
-        time.sleep(1.0)
-        button = self.driver.find_element_by_tag_name('button')
-        button.send_keys('\n')
-        time.sleep(1.0)
-        body = self.driver.find_elements_by_tag_name('body')[-1]
-        # print(body.text, "hereee")
-        # self.assertTrue('Hello World!' in body.text)
-        self.assertIn('Hello World!', body.text)
-        time.sleep(1.0)
+            time.sleep(1.0)
+            button = self.driver.find_element_by_tag_name('button')
+            button.send_keys('\n')
+            time.sleep(1.0)
+            body = self.driver.find_elements_by_tag_name('body')[-1]
+            # print(body.text, "hereee")
+            # self.assertTrue('Hello World!' in body.text)
+            self.assertIn('Hello World!', body.text)
+            time.sleep(1.0)
 
 
-    # Here we send the results to Sauce Labs and tear down our driver session
-    def tearDown(self):
-        # It's important so that you aren't billed after your test finishes.
-        self.driver.quit()
-        self.server.stop()
+        # Here we send the results to Sauce Labs and tear down our driver session
+        def tearDown(self):
+            # It's important so that you aren't billed after your test finishes.
+            self.driver.quit()
+            self.server.stop()
 
+    return TestHelloWorld
+
+TestHelloWorldChrome = define_test_suite(browser_name="Chrome")
+TestHelloWorldFirefox = define_test_suite(browser_name="Firefox")
 
 if __name__ == '__main__':
-    unittest.main(buffer=True)
+    unittest.main(buffer=True, verbosity=2)
