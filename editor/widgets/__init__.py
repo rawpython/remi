9cacd5715a1235f2b1f7f91b7dedfda5af4e7275
Davide Rosa <dddomodossola@gmail.com>
2019-11-14 14:18:13 +0100

    Editor: Additional opencv widgets. Updated icons.

--- editor/widgets/__init__.py
+++ editor/widgets/__init__.py
@@ -1,3 +1,3 @@
-from .toolbox_opencv import OpencvImRead, OpencvCrop, OpencvVideo, OpencvThreshold, OpencvSplit, OpencvBGRtoHSV
+from .toolbox_opencv import OpencvImRead, OpencvCrop, OpencvVideo, OpencvThreshold, OpencvSplit, OpencvBGRtoHSV, OpencvAddWeighted, OpencvBitwiseNot, OpencvBitwiseAnd, OpencvBitwiseOr
 from .toolbox_scheduling import TimerWidget
 from .toolbox_siemens import PLCSiemens, ButtonSetResetBit, BitStatusWidget, WordEditWidget
