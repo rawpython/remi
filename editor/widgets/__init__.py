2acf88f4f1005c19219703b11056cad4b78fdb18
Davide Rosa <dddomodossola@gmail.com>
2019-11-22 22:40:48 +0100

    Editor: fixed load external toolboxes.

--- editor/widgets/__init__.py
+++ editor/widgets/__init__.py
@@ -1,5 +1,17 @@
-from .toolbox_EPICS import EPICSBooleanButton, EPICSLed, EPICSValueMeterWidget, EPICSPlotPV
-from .toolbox_opencv import OpencvImRead, OpencvCrop, OpencvVideo, OpencvThreshold, OpencvSplit, OpencvCvtColor, OpencvAddWeighted, OpencvBitwiseNot, OpencvBitwiseAnd, OpencvBitwiseOr,\
+try:
+    from .toolbox_EPICS import EPICSBooleanButton, EPICSLed, EPICSValueMeterWidget, EPICSPlotPV
+except:
+    print("In order to use EPICS widgets install pyepics")
+
+try:
+    from .toolbox_opencv import OpencvImRead, OpencvCrop, OpencvVideo, OpencvThreshold, OpencvSplit, OpencvCvtColor, OpencvAddWeighted, OpencvBitwiseNot, OpencvBitwiseAnd, OpencvBitwiseOr,\
                             OpencvBilateralFilter, OpencvBlurFilter, OpencvDilateFilter, OpencvErodeFilter, OpencvLaplacianFilter, OpencvCanny, OpencvFindContours
+except:
+    print("In order to use OpenCv widgets install python-opencv")
+
 from .toolbox_scheduling import TimerWidget
-from .toolbox_siemens import PLCSiemens, ButtonSetResetBit, BitStatusWidget, WordEditWidget
+
+try:
+    from .toolbox_siemens import PLCSiemens, ButtonSetResetBit, BitStatusWidget, WordEditWidget
+except:
+    print("In order to use Siemens widgets install python-snap7")
