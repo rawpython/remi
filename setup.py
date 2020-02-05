f6cdd1613a14ff7660b19bbeb8e7c3e2592f1869
Davide Rosa <dddomodossola@gmail.com>
2020-02-05 10:53:55 +0100

    New version in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='remi',
-    version='2020.1.16',
+    version='2020.02.05',
     description='Python REMote Interface library',
     long_description=long_description,
     long_description_content_type="text/markdown",
