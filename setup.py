f27630721d2bbcfbce515dc2e6be3167f6a06a34
Yngve Levinsen <yngve.levinsen@ess.eu>
2020-09-11 13:01:24 +0200

    Pick up version from the git tag add remi.__version__ at runtime

--- setup.py
+++ setup.py
@@ -6,17 +6,18 @@ from setuptools import setup
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setup(name='remi',
-    version='2020.08.06',
-    description='Python REMote Interface library',
+setup(
+    name="remi",
+    description="Python REMote Interface library",
+    use_scm_version=True,
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/dddomodossola/remi',
-    download_url='https://github.com/dddomodossola/remi/archive/master.zip',
-    keywords=['gui-library','remi','platform-independent','ui','gui'],
-    author='Davide Rosa',
-    author_email='dddomodossola@gmail.com',
-    license='Apache',
+    url="https://github.com/dddomodossola/remi",
+    download_url="https://github.com/dddomodossola/remi/archive/master.zip",
+    keywords=["gui-library", "remi", "platform-independent", "ui", "gui"],
+    author="Davide Rosa",
+    author_email="dddomodossola@gmail.com",
+    license="Apache",
     packages=setuptools.find_packages(),
     include_package_data=True,
 )
