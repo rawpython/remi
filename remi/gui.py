be2a1f73992f6b1f3c2bc39cef9238dc029228f9
Davide Rosa <dddomodossola@gmail.com>
2021-08-03 09:43:43 +0200

    BugFix #457.

--- remi/gui.py
+++ remi/gui.py
@@ -1376,14 +1376,7 @@ class HEAD(Tag):
                 // characters than they actually were
                 Remi.prototype._byteLength = function(str) {
                     // returns the byte length of an utf8 string
-                    var s = str.length;
-                    for (var i=str.length-1; i>=0; i--) {
-                        var code = str.charCodeAt(i);
-                        if (code > 0x7f && code <= 0x7ff) s++;
-                        else if (code > 0x7ff && code <= 0xffff) s+=2;
-                        if (code >= 0xDC00 && code <= 0xDFFF) i--; //trail surrogate
-                    }
-                    return s;
+                    return str.length;
                 };
 
                 Remi.prototype._paramPacketize = function (ps){
