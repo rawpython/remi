889be9290ba5c38af1b3f2311af4f8f50431dbba
Davide Rosa <dddomodossola@gmail.com>
2022-11-16 09:57:52 +0100

    An example of CFC editor.

---
+++ examples/others/process_app.py
@@ -0,0 +1,347 @@
+"""
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+
+import remi.gui as gui
+import remi.server
+from remi import start, App
+import os #for path handling
+
+
+class MixinPositionSize():
+    def get_position(self):
+        return float(self.attr_x), float(self.attr_y)
+
+    def get_size(self):
+        return float(self.attr_width), float(self.attr_height)
+
+
+class MoveableWidget(gui.EventSource, MixinPositionSize):
+    container = None
+    def __init__(self, container, *args, **kwargs):
+        gui.EventSource.__init__(self)
+        self.container = container
+        self.active = False
+        self.onmousedown.do(self.start_drag, js_stop_propagation=True, js_prevent_default=True)
+            
+    def start_drag(self, emitter, x, y):
+        self.active = True
+        self.container.onmousemove.do(self.on_drag, js_stop_propagation=True, js_prevent_default=True)
+        self.container.onmouseup.do(self.stop_drag)
+        self.container.onmouseleave.do(self.stop_drag, 0, 0)
+
+    @gui.decorate_event
+    def stop_drag(self, emitter, x, y):
+        self.active = False
+        return (x, y)
+
+    @gui.decorate_event
+    def on_drag(self, emitter, x, y):
+        if self.active:
+            self.set_position(float(x) - float(self.attr_width)/2.0, float(y) - float(self.attr_height)/2.0)
+        return (x, y)
+
+
+class Input():
+    name = None
+    typ = None
+    source = None #has to be an Output
+
+    def __init__(self, name, typ = None):
+        self.name = name
+        self.typ = typ
+
+    def get_value(self):
+        return self.source.get_value()
+    
+    def link(self, output):
+        self.source = output
+
+
+class Output():
+    name = None
+    typ = None
+    destination = None #has to be an Input
+    value = None
+
+    def __init__(self, name, typ = None):
+        self.name = name
+        self.typ = typ
+
+    def get_value(self):
+        return self.value
+
+    def link(self, input):
+        self.destination = input
+        
+
+class Element():
+    name = None
+    inputs = None
+    outputs = None
+    def __init__(self, name):
+        self.name = name
+        self.inputs = []
+        self.outputs = []
+
+    def add_io(self, io):
+        if issubclass(type(io), Input):
+            self.inputs.append(io)
+        else:
+            self.outputs.append(io)
+
+
+class InputWidget(Input, gui.SvgSubcontainer, MixinPositionSize):
+    placeholder = None
+    label = None
+    def __init__(self, name, *args, **kwargs):
+        width = 10 * len(name)
+        height = 20
+        gui.SvgSubcontainer.__init__(self, 0, 0, width, height, *args, **kwargs)
+        self.placeholder = gui.SvgRectangle(0, 0, width, height)
+        self.placeholder.set_stroke(1, 'black')
+        self.placeholder.set_fill("white")
+        self.append(self.placeholder)
+
+        self.label = gui.SvgText("0%", "50%", name)
+        self.label.attr_dominant_baseline = 'middle'
+        self.label.attr_text_anchor = "start"
+        self.append(self.label)
+
+        Input.__init__(self, name, "")
+
+    def set_size(self, width, height):
+        if self.placeholder:
+            self.placeholder.set_size(width, height)
+        return super().set_size(width, height)
+
+    @gui.decorate_event
+    def onpositionchanged(self):
+        return ()
+
+
+class OutputWidget(Output, gui.SvgSubcontainer, MixinPositionSize):
+    placeholder = None
+    label = None
+    def __init__(self, name, *args, **kwargs):
+        width = 10 * len(name)
+        height = 20
+        gui.SvgSubcontainer.__init__(self, 0, 0, width, height, *args, **kwargs)
+        self.placeholder = gui.SvgRectangle(0, 0, width, height)
+        self.placeholder.set_stroke(1, 'black')
+        self.placeholder.set_fill("white")
+        self.append(self.placeholder)
+
+        self.label = gui.SvgText("100%", "50%", name)
+        self.label.attr_dominant_baseline = 'middle'
+        self.label.attr_text_anchor = "end"
+        self.append(self.label)
+
+        Output.__init__(self, name, "")
+
+    def set_size(self, width, height):
+        if self.placeholder:
+            self.placeholder.set_size(width, height)
+        return super().set_size(width, height)
+
+    @gui.decorate_event
+    def onpositionchanged(self):
+        return ()
+
+
+class Link(gui.SvgPolyline):
+    source = None
+    destination = None
+    def __init__(self, source_widget, destination_widget, *args, **kwargs):
+        gui.SvgPolyline.__init__(self, 2, *args, **kwargs)
+        self.set_stroke(1, 'black')
+        self.set_fill('transparent')
+        self.attributes['stroke-dasharray'] = "4 2"
+        self.source = source_widget
+        self.source.onpositionchanged.do(self.update_path)
+        self.destination = destination_widget
+        self.destination.onpositionchanged.do(self.update_path)
+
+        self.source.destinaton = self.destination
+        self.destination.source = self.source
+        self.update_path()
+
+    def update_path(self, emitter=None):
+        self.attributes['points'] = ''
+
+        x,y = self.source.get_position()
+        w,h = self.source.get_size()
+        xsource_parent, ysource_parent = self.source._parent.get_position()
+        wsource_parent, hsource_parent = self.destination._parent.get_size()
+
+        xsource = xsource_parent + wsource_parent
+        ysource = ysource_parent + y + h/2.0
+        self.add_coord(xsource, ysource)
+
+        x,y = self.destination.get_position()
+        w,h = self.destination.get_size()
+        xdestination_parent, ydestination_parent = self.destination._parent.get_position()
+        wdestination_parent, hdestination_parent = self.destination._parent.get_size()
+
+        xdestination = xdestination_parent
+        ydestination = ydestination_parent + y + h/2.0
+
+        offset = 10
+
+        if xdestination - xsource < offset*2:
+            self.maxlen = 6
+            """
+                    [   source]---,
+                                  |
+                        __________|
+                        |
+                        '----[destination   ]
+            """
+            self.add_coord(xsource + offset, ysource)
+
+            if ydestination > ysource:
+                #self.add_coord(xsource + offset, ysource + (ydestination - ysource)/2.0)
+                #self.add_coord(xdestination - offset, ysource + (ydestination - ysource)/2.0)
+                self.add_coord(xsource + offset, (ysource_parent + hsource_parent)  + (ydestination_parent - (ysource_parent + hsource_parent))/2.0)
+                self.add_coord(xdestination - offset, (ysource_parent + hsource_parent)  + (ydestination_parent - (ysource_parent + hsource_parent))/2.0)
+            else:
+                self.add_coord(xsource + offset, (ydestination_parent + hdestination_parent)  + (ysource_parent - (ydestination_parent + hdestination_parent))/2.0)
+                self.add_coord(xdestination - offset, (ydestination_parent + hdestination_parent)  + (ysource_parent - (ydestination_parent + hdestination_parent))/2.0)
+            self.add_coord(xdestination - offset, ydestination)
+
+        else:
+            self.maxlen = 4
+            """
+                    [   source]---,
+                                  |
+                                  '------[destination   ]
+            """
+            self.add_coord(xsource + (xdestination-xsource)/2.0, ysource)
+            self.add_coord(xdestination - (xdestination-xsource)/2.0, ydestination)
+
+        self.add_coord(xdestination, ydestination)
+
+
+class ElementWidget(Element, gui.SvgSubcontainer, MoveableWidget):
+
+    label = None
+    outline = None
+
+    def __init__(self, name, container, x, y, w, h, *args, **kwargs):
+        gui.SvgSubcontainer.__init__(self, x, y, w, h, *args, **kwargs)
+        MoveableWidget.__init__(self, container, *args, **kwargs)
+        Element.__init__(self, name)
+
+        self.outline = gui.SvgRectangle(0, 0, w, h)
+        self.outline.set_fill('lightyellow')
+        self.outline.set_stroke(2, 'black')
+        self.append(self.outline)
+
+        self.label = gui.SvgText("50%", 0, self.name)
+        self.label.attr_text_anchor = "middle"
+        self.label.attr_dominant_baseline = 'hanging'
+        self.append(self.label)
+
+    def add_io_widget(self, widget):
+        Element.add_io(self, widget)
+        self.append(widget)
+        widget.onmousedown.do(self.container.onselection_start, js_stop_propagation=True, js_prevent_default=True)
+        widget.onmouseup.do(self.container.onselection_end, js_stop_propagation=True, js_prevent_default=True)
+
+        w, h = self.get_size()
+        w_width, w_height = widget.get_size()
+
+        i = 1
+        for inp in self.inputs:
+            inp.set_position(0, (h/(len(self.inputs)+1))*i-w_height/2.0)
+            i += 1
+
+        i = 1
+        for o in self.outputs:
+            o.set_position(w - w_width, (h/(len(self.outputs)+1))*i-w_height/2.0)
+            i += 1
+
+    def set_position(self, x, y):
+        if self.inputs != None:
+            for inp in self.inputs:
+                inp.onpositionchanged()
+
+            for o in self.outputs:
+                o.onpositionchanged()
+        return super().set_position(x, y)
+
+
+class ProcessContainer(gui.Svg):
+    selected_input = None
+    selected_output = None
+
+    def __init__(self, *args, **kwargs):
+        gui.Svg.__init__(self, *args, **kwargs)
+        self.css_border_color = 'black'
+        self.css_border_width = '1'
+        self.css_border_style = 'solid'
+        self.style['background-color'] = 'lightyellow'
+
+    def onselection_start(self, emitter, x, y):
+        self.selected_input = self.selected_output = None
+        print("selection start: ", type(emitter))
+        if type(emitter) == InputWidget:
+            self.selected_input = emitter
+        else:
+            self.selected_output = emitter
+
+    def onselection_end(self, emitter, x, y):
+        print("selection end: ", type(emitter))
+        if type(emitter) == InputWidget:
+            self.selected_input = emitter
+        else:
+            self.selected_output = emitter
+
+        if self.selected_input != None and self.selected_output != None:
+            link = Link(self.selected_output, self.selected_input)
+            self.append(link)
+
+
+class MyApp(App):
+    def __init__(self, *args):
+        res_path = os.path.join(os.path.dirname(__file__), 'res')
+        super(MyApp, self).__init__(*args, static_file_path=res_path)
+
+    def idle(self):
+        pass
+
+    def main(self):
+        self.main_container = gui.VBox(width=800, height=800, margin='0px auto')
+        
+        self.process_container = ProcessContainer(width=600, height=600)
+        self.main_container.append(self.process_container)
+        
+        m = ElementWidget("Element widget 0", self.process_container, 100, 100, 200, 100)
+        m.add_io_widget(InputWidget("input0"))
+        m.add_io_widget(OutputWidget("output0"))
+        m.add_io_widget(OutputWidget("output1"))
+        self.process_container.append(m)
+
+        m = ElementWidget("Element widget 1", self.process_container, 100, 100, 200, 100)
+        m.add_io_widget(InputWidget("input0"))
+        m.add_io_widget(InputWidget("input1"))
+        m.add_io_widget(OutputWidget("output0"))
+        self.process_container.append(m)
+
+        # returning the root widget
+        return self.main_container
+
+
+    
+if __name__ == "__main__":
+    start(MyApp, debug=False, address='0.0.0.0', port=0, update_interval=0.01)
